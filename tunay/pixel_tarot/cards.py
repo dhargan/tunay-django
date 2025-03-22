fb30bba88e021cff7512bf2fd747ff1b72d03ed2
Teoman Unay <teomanunay@gmail.com>
2025-03-22 06:11:59 +0300

    Add DeckManager class for tarot card handling and update Card class for reversed display

--- tunay/pixel_tarot/cards.py
+++ tunay/pixel_tarot/cards.py
@@ -1,9 +1,34 @@
+import json
+import random
+from django.contrib.staticfiles.storage import staticfiles_storage
+
+
 class Card:
     def __init__(self, name, front_image_url, is_reversed=False):
-        self.name = name        
+        self.name = name
         self.front_image_url = front_image_url
-        self.back_image_url = 'pixel-tarot/assets/tarot__back.png'
         self.is_reversed = is_reversed
 
     def __str__(self):
-        return f"{self.name}"
+        position = "Reversed" if self.is_reversed else "Upright"
+        return f"{self.name} ({position})"
+
+
+class DeckManager:
+    def __init__(self, language='en'):
+        self.language = language
+        self.cards = self.load_tarot_cards()[language]
+
+    def load_tarot_cards(self):
+        with staticfiles_storage.open('pixel-tarot/data/tarot_cards.json') as file:
+            data = json.load(file)
+        return data
+
+    def pick_random_card(self):
+        random_card_data = random.choice(self.cards)        
+        card = Card(
+            name=random_card_data['name'],
+            front_image_url=random_card_data['front_image_url'],
+            is_reversed = random.choice([True, False])
+        )
+        return card
