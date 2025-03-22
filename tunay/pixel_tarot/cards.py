8d72b888258e7a20daede4e225655ab000196279
Teoman Unay <teomanunay@gmail.com>
2025-03-22 04:47:22 +0300

    Fix back_image_url path in Card class

--- tunay/pixel_tarot/cards.py
+++ tunay/pixel_tarot/cards.py
@@ -2,7 +2,7 @@ class Card:
     def __init__(self, name, front_image_url, is_reversed=False):
         self.name = name        
         self.front_image_url = front_image_url
-        self.back_image_url = "{% static 'pixel-tarot/assets/tarot_back.png' %}"
+        self.back_image_url = 'pixel-tarot/assets/tarot__back.png'
         self.is_reversed = is_reversed
 
     def __str__(self):
