2a2ff69ada6b706d56e28868049c21fbf41f9c7c
Teoman Unay <teomanunay@gmail.com>
2025-03-22 11:04:45 +0300

    Fix front image URL assignment in Card class and update back image path in index.html

--- tunay/pixel_tarot/cards.py
+++ tunay/pixel_tarot/cards.py
@@ -7,8 +7,7 @@ from django.templatetags.static import static
 class Card:
     def __init__(self, name, front_image_url, is_reversed=False):
         self.name = name
-        self.front_image_url = front_image_url
-        self.back_image_url = 'pixel-tarot/assets/tarot__back.png'
+        self.front_image_url = front_image_url        
         self.is_reversed = is_reversed
 
     def __str__(self):
