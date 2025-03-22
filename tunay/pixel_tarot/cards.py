300b3f98af859db0ab2787a653f1f389672e5ad5
Teoman Unay <teomanunay@gmail.com>
2025-03-22 10:59:52 +0300

    Fix back image URL in index.html and set default back image in Card class

--- tunay/pixel_tarot/cards.py
+++ tunay/pixel_tarot/cards.py
@@ -1,12 +1,14 @@
 import json
 import random
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.templatetags.static import static
 
 
 class Card:
     def __init__(self, name, front_image_url, is_reversed=False):
         self.name = name
         self.front_image_url = front_image_url
+        self.back_image_url = 'pixel-tarot/assets/tarot__back.png'
         self.is_reversed = is_reversed
 
     def __str__(self):
