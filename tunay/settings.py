2ef06621a768d6c20b6e17199588a25999e5623c
teyobeyler <teomanunay@gmail.com>
2026-08-22 19:59:31 +0300

    Add database configuration using dj-database-url in settings.py; update requirements.txt to include dj-database-url and psycopg[binary]

--- tunay/settings.py
+++ tunay/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+
+import dj_database_url
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,11 +83,15 @@ WSGI_APPLICATION = 'tunay.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        env='DATABASE_URL',
+        conn_max_age=0,
+        conn_health_checks=True,
+        ssl_require=False,
+        disable_server_side_cursors=True,
+    )
 }
+DATABASES['default'].setdefault('OPTIONS', {}).pop('pgbouncer', None)
 
 
 # Password validation
